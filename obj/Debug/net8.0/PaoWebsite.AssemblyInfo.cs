80ca2ebc343e08b027a66af4c95694ccd301f395
paorubze <paorubio@gmail.com>
2024-01-15 08:59:50 -0500

    cambios en pagina inicio

--- obj/Debug/net8.0/PaoWebsite.AssemblyInfo.cs
+++ obj/Debug/net8.0/PaoWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaoWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67e79d69ab199b9cf469c27d06f884eef8d59862")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaoWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaoWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
